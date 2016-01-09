07bf2820614047d0a3afb19b4258a3a3afbafa17
Szymon Stepniak <szymon.stepniak@gmail.com>
2016-01-09 11:00:41 +0100

    http hello world

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("Hello, World!\n");
+});
+
+server.listen(8000);
+
+console.log("Server is running at http://localhost:8000");
